94f9ea663a7ae117b2ca5ca1bff85da96fdd740a
szq233 <470774720@qq.com>
2022-02-11 13:50:13 +0800

    feat: 基础导航

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <template v-for="(item, i) in navigation">
+        <nuxt-link :key="i" :to="item.path">{{item.title}}</nuxt-link>
+        >
+      </template>
+    </div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigation: [
+        { title: '首页', path: '/' },
+        { title: '关于', path: '/about' },
+        { title: '服务支持', path: '/support' },
+      ]
+    }
+  }
+}
+</script>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,17 +1,17 @@
 <template>
-<div class="fat">
-  <p>这是首页的页面</p>
+<div class="about">
+  <p>这是关于的页面</p>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'aboutPage'
 }
 </script>
 
 <style lang="scss" scoped>
-.fat {
+.about {
 
 }
 </style>
